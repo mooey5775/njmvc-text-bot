d5ccecefea379779cc071cab76add1c3a9400a2d
Edward Li <edwardyaoli@gmail.com>
2021-04-09 01:03:01 -0400

    updated end date

--- run.py
+++ run.py
@@ -8,8 +8,7 @@ from njmvc.constants import FETCH_INTERVAL
 from secrets import ACCOUNT_SID, AUTH_TOKEN, FROM_NUMBER, TO_NUMBER
 
 START_DATE = datetime(2021, 5, 18, 12, 0, 0)
-#END_DATE = datetime(2021, 5, 22, 23, 59, 59)
-END_DATE = datetime(2021, 8, 1)
+END_DATE = datetime(2021, 5, 22, 23, 59, 59)
 
 if __name__ == '__main__':
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
